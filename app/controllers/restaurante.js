a5fc8fd8a428d35f714df8dc0e6ca2114c1d2206
Fran <francisco.sanabria@somosetica.com>
2023-05-13 15:15:36 -0400

    Agrega Add y get de restaurantes

--- app/controllers/restaurante.js
+++ app/controllers/restaurante.js
@@ -1,5 +1,25 @@
-//creamos un controlador de traer todos los restaurantes
+const { db } = require("../connection/connection");
 
 exports.getRestaurantes = async (req, res) => {
-    res.json({"hola mundo"  : "hola mundo"});
+    db.restaurante.findMany()
+        .then((restaurantes) => {
+            res.json(restaurantes);
+        })
 };
+
+exports.addRestaurante = async (req, res) => {
+    const { nombre, direccion } = req.body
+
+    try {
+        const restaurante = await db.restaurante.create({
+        data: {
+            nombre,
+            direccion,
+        },
+    })
+        res.json(restaurante)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Error al crear el restaurante.' })
+    }
+}
--- app/routes/restaurante.js
+++ app/routes/restaurante.js
@@ -8,4 +8,7 @@ const restauranteController = require('../controllers/restaurante');
 //GET /restaurante
 router.get('/', restauranteController.getRestaurantes);
 
+//POST /restaurante
+router.post('/', restauranteController.addRestaurante);
+
 module.exports = router;
