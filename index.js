5856487292c285793c175eecbc32f875ea684e86
Fran <francisco.sanabria@somosetica.com>
2023-05-13 16:41:10 -0400

    Agrega el crud de clientes

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//importar restaurantes
+//rutas
 const restauranteRoutes = require('./app/routes/restaurante');
+const clienteRoutes = require('./app/routes/cliente');
 
 app.use('/restaurantes', restauranteRoutes);
+app.use('/clientes', clienteRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
