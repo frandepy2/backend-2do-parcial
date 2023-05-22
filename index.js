5dd346047e97895358dccfb0f435f8532360c92a
Fran <francisco.sanabria@somosetica.com>
2023-05-21 22:50:43 -0400

    Agrega cors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 
 // Configura el middleware de logging
 app.use(morgan('dev')); // '
